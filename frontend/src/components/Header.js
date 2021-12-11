b2246a345d6b546c3d4fbf49bab4f40283c7be44
Vargas1723 <vargas23.bv@gmail.com>
2021-12-10 23:51:10 -0600

    Shopping Cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import { LinkContainer } from 'react-router-bootstrap' 
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React from "react"
+import { LinkContainer } from "react-router-bootstrap"
+import { Navbar, Nav, Container } from "react-bootstrap"
 
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-                <Container>
-                    <LinkContainer to="/">
-                    <Navbar.Brand>Seguimiento de programa y mejora de procesos</Navbar.Brand>
-                    </LinkContainer>
-                    
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className='ms-auto'>
-                    <LinkContainer to='/solicitud'>
-                        <Nav.Link>
-                            <i className='fas fa-file'></i> Solicitud de equipo
-                        </Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to='/login'>
-                        <Nav.Link>
-                            <i className='fas fa-user'></i> Log in
-                        </Nav.Link>
-                    </LinkContainer>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-                </Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              Seguimiento de programa y mejora de procesos
+            </Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-file'></i> Solicitud de equipo
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Log in
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header

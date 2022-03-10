0916ec1bcbd8990b852206051d297f2425ca5304
Vargas1723 <vargas23.bv@gmail.com>
2022-03-10 14:14:39 -0600

    Reviews Frontend

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -16,6 +16,9 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
 } from "../constants/productConstants"
 import { USER_DETAILS_RESET, USER_LIST_RESET, USER_LOGOUT } from "../constants/userConstants"
 
@@ -175,6 +178,41 @@ export const updateProduct = (product) => async (dispatch, getState) => {
         }
     dispatch({
       type: PRODUCT_UPDATE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+    await axios.post(`/api/products/${productId}/reviews`, review, config)
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_SUCCESS,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+        if (message === "Not authorized, token failed") {
+          dispatch(logout())
+        }
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAIL,
       payload: message,
     })
   }
